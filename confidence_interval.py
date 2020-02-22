3b8391f538796b41f45e3cf86301299e05d6ef71
Qiubei Li <qli5025@gmail.com>
2020-02-22 10:49:09 -0800

    Add argument to set interval

--- confidence_interval.py
+++ confidence_interval.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 import argparse
 import math
+import scipy.stats as st
+import numpy as np
 
+def restrictedFloat(num):
+  try:
+    num = float(num)
+  except ValueError:
+    raise argparse.ArgumentTypeError("Confidence interval {} not a floating-point literal".form(num))
+
+  if num < 0.0 or num > 1.0:
+    raise argparse.ArgumentTypeError("Confidence interval {} not in range [0.0, 1.0]".format(num))
+  return num
 
 parser = argparse.ArgumentParser()
 parser.add_argument("mean", type=float, help = "sample mean")
 parser.add_argument("deviation", type=float, help ="standard deviation")
 parser.add_argument("n", type=int, help ="sample size")
-parser.add_argument("-i", "--interval", type= int, help = "confidence interval ", choices=[80,90,95,99], default = 95)
+parser.add_argument(
+  "-i", "--interval",
+  type=restrictedFloat,
+  help = "Confidence interval in decimal form between 0.0 and 1.0",
+  default = 0.95
+)
 args = parser.parse_args()
 error= args.deviation/math.sqrt(args.n)
+proportion = (1-args.interval)/2
 
-z_star={
-80: 1.282,
-90: 1.645,
-95: 1.96,
-99: 2.576
-}
+z_star= math.fabs(st.norm.ppf(proportion))
 
-new_interval = z_star[args.interval]*error
+new_interval = z_star*error
 
 print("{} \u00B1 {}".format(args.mean, new_interval))
