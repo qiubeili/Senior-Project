059b1587f6174aba16a6fad2181fd2ea6043fcee
Qiubei Li <qli5025@gmail.com>
2020-05-17 12:03:06 -0700

    Make graph self updating and refine self updating inputs

--- confidence_interval.py
+++ confidence_interval.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.figure import Figure
 
 #dictionary for z star values
-interval = {
+interval_num_to_value = {
     1: 1.282,
     2: 1.440,
     3: 1.645,
@@ -18,71 +18,100 @@ interval = {
 }
 
 #function to calculate the confidence interval + display on app
-def calculate_interval():
+def calculate_interval(sample_mean, standard_deviation, sample_size, interval):
     #obtain inputs from sample mean, standard deviation, and sample size
     sample_mean_value = sample_mean_input.get()
     standard_deviation_value = standard_deviation_input.get()
     sample_size_value = sample_size_input.get()
-    idek = interval_num_to_value.get()
-    if (not (sample_mean_value and standard_deviation_value and sample_size_value and idek)):
-        return
+    interval = interval_var.get()
+    if (not (sample_mean_value and standard_deviation_value and sample_size_value and interval)):
+        return None
     #do math
     error = float(standard_deviation_value)/math.sqrt(float(sample_size_value))
-    #calculate the z star value
-    for x in interval:
-        x = idek
-        z_star= interval[x]
+    #get the z star value
+    z_star = interval_num_to_value[interval]
     #do more math
     new_interval = float(z_star) * error
+    return {
+        'sample_mean': sample_mean_value,
+        'standard_deviation': standard_deviation_value,
+        'interval': new_interval
+    }
+
+class graphing:
+    def __init__(self):
+        self.graph = None
+
+    def reset_graph(self):
+        if self.graph is None:
+            return
+        self.graph.destroy()
+        # This is important for the if condition to work in update_graph
+        self.graph = None
+    def close_graph(self):
+        if self.graph is None:
+            return
+        self.graph.destroy()
+        # This is important for the if condition to work in update_graph
+        self.graph = None
+
+    def update_graph(self, result):
+        if (self.graph is not None):
+            graph.reset_graph()
+        else:
+            self.graph = tkinter.Toplevel()
+
+        sample_mean_value = result.get('sample_mean')
+        new_interval = result.get('interval')
+        standard_deviation_value = result.get('standard_deviation')
+
+        #graphing part
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        x = np.linspace(float(sample_mean_value) - 3 * float(standard_deviation_value), float(sample_mean_value) + 3 * float(standard_deviation_value), 10000)
+        nVals = [norm.pdf(i,float(sample_mean_value),float(standard_deviation_value)) for i in x]
+        ax.plot(x,nVals)
+        x1 = float(sample_mean_value) - new_interval
+        x2 = float(sample_mean_value) + new_interval
+        ax.fill_between(x,nVals,color = '#111111',where = (x > x1) & (x < x2))
+
+        canvas = FigureCanvasTkAgg(fig, master= self.graph)
+        canvas.draw()
+        toolbar = NavigationToolbar2Tk(canvas, self.graph)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+graph = graphing()
+
+def update_result_label(result):
     #setting string display
-    string_to_display = "Confidence Interval:  " + sample_mean_value + " \u00B1 " + str(new_interval)
-    var_1.set(string_to_display)
-#function to graph out confidence interval calculated
-def graphing():
-    graph = tkinter.Toplevel()
+    string_to_display = "Confidence Interval: {sample_mean} \u00B1 {interval}".format(**result)
+    confidence_interval_var.set(string_to_display)
 
-    sample_mean_value = sample_mean_input.get()
-    standard_deviation_value = standard_deviation_input.get()
-    sample_size_value = sample_size_input.get()
-    error = float(standard_deviation_value)/math.sqrt(float(sample_size_value))
+def clear_result_label():
+    confidence_interval_var.set("")
 
-    for x in interval:
-        x = interval_num_to_value.get()
-        z_star= interval[x]
-
-    new_interval = float(z_star) * error
-    #graphing part
-    fig = Figure(figsize=(5, 4), dpi=100)
-    ax = fig.add_subplot(111)
-    x = np.linspace(float(sample_mean_value) - 3 * float(standard_deviation_value), float(sample_mean_value) + 3 * float(standard_deviation_value), 10000)
-    nVals = [norm.pdf(i,float(sample_mean_value),float(standard_deviation_value)) for i in x]
-    ax.plot(x,nVals)
-    x1 = float(sample_mean_value) - new_interval
-    x2 = float(sample_mean_value) + new_interval
-    ax.fill_between(x,nVals,color = '#111111',where = (x > x1) & (x < x2))
-
-    canvas = FigureCanvasTkAgg(fig, master= graph)
-    canvas.draw()
-    toolbar = NavigationToolbar2Tk(canvas, graph)
-    toolbar.update()
-
-    button = tkinter.Button(master=graph, text="Quit", command=graph.destroy)
-
-    button.pack(side=tkinter.BOTTOM)
-    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-def handle_user_input(event):
-    print(event)
-    calculate_interval()
-
+def handle_values_changed(*args):
+    sample_mean = sample_mean_input.get()
+    standard_deviation = standard_deviation_input.get()
+    sample_size = sample_size_input.get()
+    interval = interval_var.get()
+    result = calculate_interval(sample_mean, standard_deviation, sample_size, interval)
+    if (result is not None):
+        update_result_label(result)
+        graph.update_graph(result)
+    else:
+        clear_result_label()
+        graph.close_graph()
 
 #basic setup
 root = tkinter.Tk()
 root.title("Confidence Interval Calculator")
 
 #variables used
-var_1 = tkinter.StringVar()
-interval_num_to_value= tkinter.IntVar()
+confidence_interval_var = tkinter.StringVar()
+interval_var= tkinter.IntVar()
+interval_var.trace('w', handle_values_changed)
 
 #frames to group stuff
 frame_labels = tkinter.Frame(root) #sample mean, standard deviation, and sample size
@@ -94,35 +123,27 @@ frame_answer = tkinter.Frame(root) #answer
 sample_mean = tkinter.Label(frame_labels, text = 'Sample Mean',).grid(row = 0)
 standard_deviation = tkinter.Label(frame_labels, text = 'Standard Deviation').grid(row=1)
 sample_size = tkinter.Label(frame_labels, text = 'Sample Size').grid(row=2)
-sample_mean_input = tkinter.Entry(frame_labels)
-sample_mean_input.bind('<KeyRelease>', handle_user_input)
-standard_deviation_input = tkinter.Entry(frame_labels)
-standard_deviation_input.bind('<KeyRelease>', handle_user_input)
-sample_size_input = tkinter.Entry(frame_labels)
-sample_size_input.bind('<KeyRelease>', handle_user_input)
+sample_mean_input_var = tkinter.StringVar()
+sample_mean_input_var.trace('w', handle_values_changed)
+sample_mean_input = tkinter.Entry(frame_labels, textvariable=sample_mean_input_var)
+standard_deviation_input_var = tkinter.StringVar()
+standard_deviation_input_var.trace('w', handle_values_changed)
+standard_deviation_input = tkinter.Entry(frame_labels, textvariable=standard_deviation_input_var)
+sample_size_input_var = tkinter.StringVar()
+sample_size_input_var.trace('w', handle_values_changed)
+sample_size_input = tkinter.Entry(frame_labels, textvariable=sample_size_input_var)
 
 #confidence interval choices between 80-99.9%
-interval_1 = tkinter.Radiobutton(frame_choice, text = "80%", value = 1, variable = interval_num_to_value)
-interval_1.bind('<ButtonRelease>', handle_user_input)
-interval_2 = tkinter.Radiobutton(frame_choice, text = "85%", value = 2, variable = interval_num_to_value)
-interval_2.bind('<ButtonRelease>', handle_user_input)
-interval_3 = tkinter.Radiobutton(frame_choice, text = "90%", value = 3, variable = interval_num_to_value)
-interval_3.bind('<ButtonRelease>', handle_user_input)
-interval_4 = tkinter.Radiobutton(frame_choice, text = "95%", value = 4, variable = interval_num_to_value)
-interval_4.bind('<ButtonRelease>', handle_user_input)
-interval_5 = tkinter.Radiobutton(frame_choice, text = "99%", value = 5, variable = interval_num_to_value)
-interval_5.bind('<ButtonRelease>', handle_user_input)
-interval_6 = tkinter.Radiobutton(frame_choice, text = "99.5%", value = 6, variable = interval_num_to_value)
-interval_6.bind('<ButtonRelease>', handle_user_input)
-interval_7 = tkinter.Radiobutton(frame_choice, text = "99.9%", value = 7, variable = interval_num_to_value)
-interval_7.bind('<ButtonRelease>', handle_user_input)
-
-#calculate button
-calculate_button = tkinter.Button(frame_button, text = 'Calculate', command = calculate_interval)
-graph_button = tkinter.Button(frame_button, text = 'Show Graph', command = graphing)
+interval_1 = tkinter.Radiobutton(frame_choice, text = "80%", value = 1, variable = interval_var)
+interval_2 = tkinter.Radiobutton(frame_choice, text = "85%", value = 2, variable = interval_var)
+interval_3 = tkinter.Radiobutton(frame_choice, text = "90%", value = 3, variable = interval_var)
+interval_4 = tkinter.Radiobutton(frame_choice, text = "95%", value = 4, variable = interval_var)
+interval_5 = tkinter.Radiobutton(frame_choice, text = "99%", value = 5, variable = interval_var)
+interval_6 = tkinter.Radiobutton(frame_choice, text = "99.5%", value = 6, variable = interval_var)
+interval_7 = tkinter.Radiobutton(frame_choice, text = "99.9%", value = 7, variable = interval_var)
 
 #The confidence interval is....
-answer = tkinter.Label(frame_answer, textvariable = var_1)
+answer = tkinter.Label(frame_answer, textvariable = confidence_interval_var)
 
 #input orientation
 sample_mean_input.grid(row = 0, column = 1)
@@ -138,12 +159,8 @@ interval_5.grid(row=3, column = 4)
 interval_6.grid(row=3, column = 5)
 interval_7.grid(row=3, column = 6)
 
-#calculate button orientation
-calculate_button.grid(row=4, column = 0)
-graph_button.grid(row=4, column = 1)
-
 #answer orientation
-answer.grid(row=5)
+answer.grid(row=4)
 
 #frame orientation
 frame_labels.grid(column = 0, row = 0)
